9f2cc37ab6f148d2c55e7b47d7c5146189ee2745
CalebTVanDyke <ctvandyke24@gmail.com>
2013-11-07 15:48:24 -0600

    regex

--- project2/filehandling.cpp
+++ project2/filehandling.cpp
@@ -42,18 +42,18 @@ board stringToBoard(std::string fileInfo){
 	//Handles retrieval of Terrain information from the string containing the file
 	regex_search(fileInfo, container, reTerrain);
 	if(container.size() != 1){
-		cerr << "Error with the terrain portion" << '\n';
+		std::cerr << "Error with the terrain portion" << '\n';
 		throw -1;
 	}
 	smatch xrange;
 	regex_search(container[0].str(), xrange, reXrange);
 	if(xrange.size() != 1){
-		cerr << "Error with the Xrange portion of terrain" << '\n';
+		std::cerr << "Error with the Xrange portion of terrain" << '\n';
 		throw -1;
 	}
 	regex_search(xrange[0].str(), xrange, reLowHigh);
 	if(xrange.size() != 1){
-		cerr << "Error with the Xrange portion of terrain" << '\n';
+		std::cerr << "Error with the Xrange portion of terrain" << '\n';
 		throw -1;
 	}
 	std::string x = xrange.str();
@@ -61,12 +61,12 @@ board stringToBoard(std::string fileInfo){
 	smatch yrange;
 	regex_search(container[0].str(), yrange, reYrange);
 	if(xrange.size() != 1){
-		cerr << "Error with the Yrange portion of terrain" << '\n';
+		std::cerr << "Error with the Yrange portion of terrain" << '\n';
 		throw -1;
 	}
 	regex_search(yrange[0].str(), yrange, reLowHigh);
 	if(xrange.size() != 1){
-		cerr << "Error with the Yrange portion of terrain" << '\n';
+		std::cerr << "Error with the Yrange portion of terrain" << '\n';
 		throw -1;
 	}
 	std::string y = yrange.str();
@@ -76,18 +76,18 @@ board stringToBoard(std::string fileInfo){
 	int ylow; 
 	int yhigh;
 	if((sscanf(x.c_str(), "%d..%d", &xlow, &xhigh)) != 2){
-		cerr << "Error reading range of x values" << '\n';
+		std::cerr << "Error reading range of x values" << '\n';
 		throw -1;
 	}
 	if((sscanf(y.c_str(), "%d..%d", &ylow, &yhigh)) != 2){
-		cerr << "Error reading range of y values" << '\n';
+		std::cerr << "Error reading range of y values" << '\n';
 		throw -1;
 	}
 
 	//Handles retrieval of Chars information from the string containing the file
 	regex_search(fileInfo, container, reChars);
 	if(container.size() != 1){
-		cerr << "Error with the Chars portion" << '\n';
+		std::cerr << "Error with the Chars portion" << '\n';
 		throw -1;
 	}
 	int alive = 64, dead = 126;
@@ -95,21 +95,21 @@ board stringToBoard(std::string fileInfo){
 	char * aliveLoc = strstr(container.str().c_str(), "Alive=");
 	if(aliveLoc){
 		if((err = sscanf(aliveLoc, "Alive=%d", &alive)) != 1){
-			cerr << "Error reading Alive value from Chars portion, default will be used" << '\n';
+			std::cerr << "Error reading Alive value from Chars portion, default will be used" << '\n';
 			alive = 64;
 		}
 	}else{
-		cerr << "Could not find location of Alive value, default will be used" << '\n';
+		std::cerr << "Could not find location of Alive value, default will be used" << '\n';
 		alive = 64;
 	}
 	char * deadLoc = strstr(container.str().c_str(), "Dead=");
 	if(deadLoc){
 		if((sscanf(deadLoc, "Dead=%d", &dead)) != 1){
-			cerr << "Error reading Dead value from Chars portion. default will be used" << '\n';
+			std::cerr << "Error reading Dead value from Chars portion. default will be used" << '\n';
 			dead = 126;
 		}
 	}else{
-		cerr << "Could not find location of Dead value, default will be used" << '\n';
+		std::cerr << "Could not find location of Dead value, default will be used" << '\n';
 		dead = 126;
 	}
 
@@ -123,12 +123,12 @@ board stringToBoard(std::string fileInfo){
 		smatch xrange;
 		regex_search(container[0].str(), xrange, reXrange);
 		if(xrange.size() != 1){
-			cerr << "Error with the Xrange portion of terrain" << '\n';
+			std::cerr << "Error with the Xrange portion of terrain" << '\n';
 			throw -1;
 		}
 		regex_search(xrange[0].str(), xrange, reLowHigh);
 		if(xrange.size() != 1){
-			cerr << "Error with the Xrange portion of terrain" << '\n';
+			std::cerr << "Error with the Xrange portion of terrain" << '\n';
 			throw -1;
 		}
 		std::string x = xrange.str();
@@ -136,12 +136,12 @@ board stringToBoard(std::string fileInfo){
 		smatch yrange;
 		regex_search(container[0].str(), yrange, reYrange);
 		if(xrange.size() != 1){
-			cerr << "Error with the Yrange portion of terrain" << '\n';
+			std::cerr << "Error with the Yrange portion of terrain" << '\n';
 			throw -1;
 		}
 		regex_search(yrange[0].str(), yrange, reLowHigh);
 		if(xrange.size() != 1){
-			cerr << "Error with the Yrange portion of terrain" << '\n';
+			std::cerr << "Error with the Yrange portion of terrain" << '\n';
 			throw -1;
 		}
 		std::string y = yrange.str();
@@ -151,28 +151,28 @@ board stringToBoard(std::string fileInfo){
 		int ylow = gameBoard.getYMin();
 		int yhigh = gameBoard.getYMax();
 		if((sscanf(x.c_str(), "%d..%d", &xlow, &xhigh)) != 2){
-			cerr << "Error reading range of x values, default will be used" << '\n';
+			std::cerr << "Error reading range of x values, default will be used" << '\n';
 			xlow = gameBoard.getXMin();
 			xhigh = gameBoard.getXMax();
 		}
 		if(xlow < gameBoard.getXMin()){
-			cerr << "Window X-Low was out of range of the terrain, default will be used\n";
+			std::cerr << "Window X-Low was out of range of the terrain, default will be used\n";
 			xlow = gameBoard.getXMin();
 		}
 		if(xhigh > gameBoard.getXMax()){
-			cerr << "Window X-High was out of range of the terrain, default will be used\n";
+			std::cerr << "Window X-High was out of range of the terrain, default will be used\n";
 			xhigh = gameBoard.getXMax();
 		}
 		if((sscanf(y.c_str(), "%d..%d", &ylow, &yhigh)) != 2){
-			cerr << "Error reading range of y values" << '\n';
+			std::cerr << "Error reading range of y values" << '\n';
 			throw -1;
 		}
 		if(ylow < gameBoard.getYMin()){
-			cerr << "Window Y-Low was out of range of the terrain, default will be used\n";
+			std::cerr << "Window Y-Low was out of range of the terrain, default will be used\n";
 			ylow = gameBoard.getYMin();
 		}
 		if(yhigh > gameBoard.getYMax()){
-			cerr << "Window Y-High was out of range of the terrain, default will be used\n";
+			std::cerr << "Window Y-High was out of range of the terrain, default will be used\n";
 			yhigh = gameBoard.getYMax();
 		}
 
@@ -183,7 +183,7 @@ board stringToBoard(std::string fileInfo){
 	//Handles retrieval of Colors information from the string containing the file
 	regex_search(fileInfo, container, reColors);
 	if(container.size() != 1){
-		cerr << "Error with the Colors portion" << '\n';
+		std::cerr << "Error with the Colors portion" << '\n';
 		throw -1;
 	}
 	smatch aliveMatch;
@@ -199,24 +199,24 @@ board stringToBoard(std::string fileInfo){
 
 		regex_search(aliveMatch[0].str(), aliveMatch, reParens);
 		if(aliveMatch.size() != 1){ 
-			cerr << "Error attempting to read Alive color value from Colors portion, default will be used" << '\n';
+			std::cerr << "Error attempting to read Alive color value from Colors portion, default will be used" << '\n';
 			gameBoard.setAliveColor(255, 255, 255);
 		}
 		aliveColor = aliveMatch.str();
 		int r, b ,g;
 		if((sscanf(aliveColor.c_str(), "(%d,%d,%d)", &r, &g, &b)) != 3){
-			cerr << "RBG value for Alive in Colors portion was not valid, default will be used" << '\n';
+			std::cerr << "RBG value for Alive in Colors portion was not valid, default will be used" << '\n';
 			gameBoard.setAliveColor(255, 255, 255);
 		}
 		if(r < 0 || r > 255 || b < 0 || b > 255 || g < 0 || g > 255){
-			cerr << "RBG value for Alive in Colors portion was not valid, default will be used" << '\n';
+			std::cerr << "RBG value for Alive in Colors portion was not valid, default will be used" << '\n';
 			gameBoard.setAliveColor(255, 255, 255);
 		}else{
 			gameBoard.setAliveColor(r, g, b);
 		}
 		
 	}else{
-		cout << "Alive portion not found, default color will be used\n";
+		std::cerr << "Alive portion not found, default color will be used\n";
 		gameBoard.setAliveColor(255, 255, 255);
 	}
 
@@ -225,29 +225,29 @@ board stringToBoard(std::string fileInfo){
 	if(deadMatch.size() == 1){
 		regex_search(deadMatch[0].str(), deadMatch, reParens);
 		if(deadMatch.size() != 1){
-			cerr << "Error attempting to read Dead color value from Colors portion, default will be used" << '\n';
+			std::cerr << "Error attempting to read Dead color value from Colors portion, default will be used" << '\n';
 			gameBoard.setDeadColor(64 , 64, 64);
 		}
 		deadColor = deadMatch.str();
 		if((sscanf(deadColor.c_str(), "(%d,%d,%d)", &r, &g, &b)) != 3){
-			cerr << "RBG value for Dead in Colors portion was not valid, default will be used." << '\n';
+			std::cerr << "RBG value for Dead in Colors portion was not valid, default will be used." << '\n';
 			gameBoard.setDeadColor(64 , 64, 64);
 		}
 		else if(r < 0 || r > 255 || b < 0 || b > 255 || g < 0 || g > 255){
-			cerr << "RBG value for Dead in Colors portion was not valid" << '\n';
+			std::cerr << "RBG value for Dead in Colors portion was not valid" << '\n';
 			gameBoard.setDeadColor(64 , 64, 64);
 		}else{
 			gameBoard.setDeadColor(r, g, b);
 		}
 	}else{
-		cerr << "Dead color not found default will be used\n";
+		std::cerr << "Dead color not found default will be used\n";
 		gameBoard.setDeadColor(0 , 0, 0);
 	}
 
 	//Handles retrieval of Initial information from the string containing the file
 	regex_search(fileInfo, container, reInitial);
 	if(container.size() != 1){
-		cerr << "Error with Initial portion\n";
+		std::cerr << "Error with Initial portion\n";
 		throw -1;
 	}
 	std::string temp = container.str();
@@ -258,11 +258,11 @@ board stringToBoard(std::string fileInfo){
 		int xc;
 		int yc;
 		if((sscanf(regit->str().c_str(), "(%d, %d)", &xc, &yc)) != 2){
-			cerr << "Error reading X, Y coordinate" << '\n';
+			std::cerr << "Error reading X, Y coordinate" << '\n';
 			continue;
 		}
 		if(xc < xlow || xc > xhigh || yc < ylow || yc > yhigh){
-			cerr << "Coordinate in " << regit->str() << " was not in range of the terrain." << '\n';
+			std::cerr << "Coordinate in " << regit->str() << " was not in range of the terrain." << '\n';
 			continue;
 		}
 		gameBoard.setAlive(xc, yc);
@@ -283,7 +283,7 @@ std::string getStringFromFile(const char *filename){
 		inf.close();
 		return fileData;
 	}else{
-		cerr << "Error reading file: " << filename << '\n';
+		std::cerr << "Error reading file: " << filename << '\n';
 		throw -1;
 	}
 }
@@ -292,14 +292,14 @@ std::string getStringFromStdIn(){
 		std::string fileData = "";
 		char c;
 		int i = 0;
-		cin.get(c);
-		while(!cin.eof()){
+		std::cin.get(c);
+		while(!std::cin.eof()){
 			fileData += c;
-			cin.get(c);
+			std::cin.get(c);
 		}
 		return fileData;
 	}catch(int e){
-		cerr << "Error getting string from stdin\n";
+		std::cerr << "Error getting string from stdin\n";
 		throw -1;
 	}
 }
